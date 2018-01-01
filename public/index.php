beb3aba04a3551953b67020a498469c52d4a3848
Cameron Smith <gitcontrol@outlook.com>
2018-01-01 16:16:42 +0000

    Added basic routing

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/access-token', function() {
+        echo 'access token';
+    });
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $handler();
+        break;
+    default:
+        http_response_code(404);
+        die();
+        break;
+}
